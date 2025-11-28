9d65278f9cd97ead6ee867fb0546a2088e7a463b
abdulkatherH <sheikabdulkather.h@gmail.com>
2025-11-28 11:54:42 +0530

    Average with Dictionary

--- programs/learn9.py
+++ programs/learn9.py
@@ -29,3 +29,30 @@ for i in range(count):
 
 avg = sum(numbers) / count
 print("Average: ",avg)
+
+# Average of Even and Odd Numbers Separately
+print('--------------------- AVERAGE OF ODD EVEN -----------------')
+numbers = [1,2,3,4,5,6]
+even = [n for n in numbers if n % 2 == 0] 
+odd = [n for n in numbers if n % 2 == 1]
+print("Even: ",even)
+print("Odd: ",odd)
+avg_even = sum(even) / len(even)
+avg_odd = sum(odd) / len(odd)
+
+print("Average of even: ", avg_even)
+print("Average of odd: ", avg_odd)
+
+# Average Using stastics Module
+print('--------------------- AVERAGE stastics -----------------')
+import statistics
+nums = [20,20]
+print("Average statistics: ",statistics.mean(nums))
+
+# Average of values in a Dictionary
+print('--------------------- Average Dictionary -----------------')
+marks = {"math": 10, "science": 10, "english": 10}
+print("sum: ",sum(marks.values()))
+print("len: ",len(marks))
+avg = sum(marks.values()) / len(marks)
+print("Average marks: ", avg)

dbd334bcfee79dedc93bf4f20d4630ef575f58ed
abdulkatherH <sheikabdulkather.h@gmail.com>
2025-11-28 10:22:32 +0530

    Average by list and user input

--- programs/learn9.py
+++ programs/learn9.py
@@ -1,4 +1,5 @@
 # Implementing variable length arguments in python
+print('--------------------- Tuple -----------------')
 def average(*t):
     print('sum: ',sum(t))
     print('length: ',len(t))
@@ -9,3 +10,22 @@ result2 = average(5,10)
 
 print('AVERAGE 1: ',result1)
 print('AVERAGE 2: ',result2)
+
+# Average of numbers in a list
+print('--------------------- List -----------------')
+def average_list(numbers):
+    return sum(numbers) / len(numbers)
+
+nums = [10,20,30,40]
+print("AVERAGE IN A LIST: ", average_list(nums))
+
+# Average using user Input
+print('--------------------- USER INPUT -----------------')
+count = int(input("How many numbers? "))
+numbers = []
+for i in range(count):
+    num = float(input("Enter the Number: "))
+    numbers.append(num)
+
+avg = sum(numbers) / count
+print("Average: ",avg)

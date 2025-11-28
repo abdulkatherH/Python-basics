bac8c768f535ad607c83b021e4f9f409dc593164
abdulkatherH <sheikabdulkather.h@gmail.com>
2025-11-28 22:34:20 +0530

    Getting Frequency of each number

--- programs/learn10.py
+++ programs/learn10.py
@@ -7,4 +7,12 @@ def bubble_sort(arr):
                 arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
-print(bubble_sort([5,2,8,1,9]))
+print('SORTING THE NUMBERS: ',bubble_sort([5,2,8,1,9]))
+
+# Getting frequency of each number
+def frequency(arr):
+    freq = {}
+    for item in arr:
+        freq[item] = freq.get(item, 0) + 1
+    return freq
+print('FREQUENCY OF EACH NUMBER: ', frequency([1,2,3,3,4,4,4,4,4,5,5]))

5ed0a9d09fabf3cf9b40bdbcbf27d917a3cd9b86
abdulkatherH <sheikabdulkather.h@gmail.com>
2025-12-04 18:28:14 +0530

    decorator samples

---
+++ programs/learn11.py
@@ -0,0 +1,11 @@
+def decorator(func):
+    def wrapper():
+        print('Before function call')
+        func()
+        print('After function call')
+    return wrapper
+
+@decorator
+def greet():
+    print('Hello')
+greet()

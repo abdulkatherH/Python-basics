96d8ddcffb979f1b4b6b580c989a4e27499b86c3
abdulkatherH <sheikabdulkather.h@gmail.com>
2025-12-23 16:07:05 +0530

    Second duplicate number in list

--- programs/Q1.py
+++ programs/Q1.py
@@ -12,3 +12,19 @@
 # Question-3
 # pattern = [1, [2, 3], 4, 5]
 # Flatern this list without using forloop
+
+# Question-4
+# Second duplicate number in list
+numberList = [12, 3, 55, 23, 6, 78, 33, 4]
+max1 = numberList[0]
+max2 = numberList[0]
+print('max1: ',max1)
+print('max2: ',max2)
+for num in numberList:
+    if num > max1:
+        max2 = max1
+        max1 = num
+    elif num > max2 and num != max1:
+        max2 = num
+
+print("Second maximum number:", max2)

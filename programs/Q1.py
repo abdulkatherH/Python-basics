9f0aec77e0ddfee0adc7a8fe48a927db4f3f98d4
abdulkatherH <sheikabdulkather.h@gmail.com>
2025-12-23 14:53:38 +0530

    interview programs

---
+++ programs/Q1.py
@@ -0,0 +1,14 @@
+# Question-1
+# Need out put like this
+# 1 
+# 3 3 
+# 2 2 2
+# 5 5 5 5
+# 4 4 4 4 4
+
+# Question-2
+# without using numbers print 1 to 100
+
+# Question-3
+# pattern = [1, [2, 3], 4, 5]
+# Flatern this list without using forloop
